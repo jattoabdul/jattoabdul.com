2cc8595551f5908e15bdbec353dd8120a7725176
jattoabdul <jattoade@gmail.com>
2025-01-27 23:13:38 -0500

    chore: setup shadcn and test it works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { TestComponent } from '@components/test/component';
+
 export default function Home() {
-  return <main className="min-h-screen">{/* Hero Section goes in here */}</main>;
+  return (
+    <main className="min-h-screen">
+      {/* Hero Section goes in here */}
+      <TestComponent />
+    </main>
+  );
 }
---
+++ src/components/test/component.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@components/ui/button';
+
+export const TestComponent = () => {
+  return (
+    <div>
+      Test Component <Button>Button</Button>
+    </div>
+  );
+};
