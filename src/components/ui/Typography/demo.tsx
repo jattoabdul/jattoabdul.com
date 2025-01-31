d1d2f72c1cd94f1647e19ea74d672c235a7b1fb5
jattoabdul <jattoade@gmail.com>
2025-01-30 21:34:17 -0500

    feat(ui): update the typography demo

--- src/components/ui/Typography/demo.tsx
+++ src/components/ui/Typography/demo.tsx
@@ -1,7 +1,7 @@
 export const Typography = () => {
   return (
-    <div>
-      <h1>Hello World Test Component</h1>
+    <div className={`p-14 space-y-14`}>
+      <h1 className="font-avant-garde text-center">Typography Demo</h1>
       <h1 className="flex flex-col items-center text-9xl text-center uppercase font-avant-garde font-bold">
         <div className="line flex">
           <div className="char">M</div>
